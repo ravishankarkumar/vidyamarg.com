f1ef357c8a5babee449f9a1c508d2687d177a689
Ravi Shankar <ravishankar@Ravis-Personal-Macbook-pro.local>
2023-10-27 06:54:52 +0530

    youtube videos link on course page

---
+++ _next/static/chunks/app/upsc/courses/indian-society/page-154251077dc167d6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[863],{3615:function(e,n,s){Promise.resolve().then(s.bind(s,8165)),Promise.resolve().then(s.bind(s,3238)),Promise.resolve().then(s.bind(s,570)),Promise.resolve().then(s.bind(s,4156)),Promise.resolve().then(s.bind(s,1507))}},function(e){e.O(0,[317,600,342,507,971,472,744],function(){return e(e.s=3615)}),_N_E=e.O()}]);
